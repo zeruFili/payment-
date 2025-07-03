dfb9e4de2426354a644affb1d1e5240d17457057
zeruFili <zerubabelfilmon1@gmail.com>
2025-07-03 23:55:32 +0300

    created the model for the video

---
+++ models/video.model.js
@@ -0,0 +1,42 @@
+// models/Video.js
+const mongoose = require('mongoose');
+
+const videoSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    youtubeURL: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(url) {
+                return url.startsWith('https://www.youtube.com/') || url.startsWith('https://youtube.com/') || url.startsWith('https://youtu.be/');
+            },
+            message: 'Invalid YouTube URL format'
+        }
+    },
+    videoName: {
+        type: String,
+        required: true,
+        maxlength: 100, // Optional: limit the length of the video name
+    },
+    amount: {
+        type: Number,
+        required: true,
+        min: 0,
+    },
+    message: {
+        type: String,
+        maxlength: 500,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Video = mongoose.model('Video', videoSchema);
+
+module.exports = Video;
